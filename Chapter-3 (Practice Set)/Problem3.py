4ed88605a87a216fd294255364fe8445e254f2e5
Anupam Lata Yadav <yanu77662@gmail.com>
2025-10-31 17:37:06 +0530

    Implement double space detection and replacement
    Add functionality to detect and replace double spaces in a string.

---
+++ Chapter-3 (Practice Set)/Problem3.py
@@ -0,0 +1,7 @@
+# to detect the double space in a string
+name = "Anupam is a  good girl and "
+print(name.find("  "))
+
+# replace the double space from problem 3 with single spaces.
+
+print (name.replace("  ",  " "))
