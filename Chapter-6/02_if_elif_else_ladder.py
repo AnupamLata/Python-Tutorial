b161cccbfb69a0d14a10b1cd9b906cc29cdc59aa
Anupam Lata Yadav <yanu77662@gmail.com>
2025-11-03 00:13:28 +0530

    Create 02_if_elif_else_ladder.py

---
+++ Chapter-6/02_if_elif_else_ladder.py
@@ -0,0 +1,20 @@
+a = int(input("Enter your age: "))
+
+# if elif else ladder
+
+if  (a >= 18):
+
+    print("you are above the age of consert")
+    print("good for you")
+
+elif(a<0):
+    print("you are entering an invalid age")
+
+elif (a==0):
+    print("you are entering 0 which is not valid age")        
+
+else:
+    print("you are below the age of consert")  
+
+
+print("end of program")     
