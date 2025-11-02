61724f435f7742b89c085dc8e076004e4660a244
Anupam Lata Yadav <yanu77662@gmail.com>
2025-11-03 00:12:07 +0530

    Create 01_Conditionals.py

---
+++ Chapter-6/01_Conditionals.py
@@ -0,0 +1,16 @@
+a = int(input("Enter your age: "))
+
+# if else statement 4
+
+
+if  (a >= 18):
+
+    print("you are above the age of consert")
+    print("good for you")
+      
+
+else:
+    print("you are below the age of consert")  
+
+
+print("end of program")     
