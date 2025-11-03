5efdd1cf7b3af3960dc8051e0e22e5e3fec0ff44
Anupam Lata Yadav <yanu77662@gmail.com>
2025-11-04 00:06:28 +0530

    Add name presence checker in Problem5.py
    Implement a program to check name presence in a list.

---
+++ Chapter-6 (Practice Set)/Problem5.py
@@ -0,0 +1,11 @@
+# Write a program which finds out whether a given name is present in a list or not.
+
+l = ["Harry", "Anu", "Aniket", "Rohan", "Divya"]
+
+name = input("Enter your name: ")
+
+if(name in l):
+    print("your name is in  the list")
+
+else:
+    print("your name is not in the list")    
