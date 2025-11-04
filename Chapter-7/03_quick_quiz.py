55f92677341b6fe65ce712644e5d46e69d2dedd2
Anupam Lata Yadav <yanu77662@gmail.com>
2025-11-05 00:50:58 +0530

    Add quick quiz program using while loop

---
+++ Chapter-7/03_quick_quiz.py
@@ -0,0 +1,24 @@
+# Write a program to print 1 to 50 using a while loop.
+# i = 1
+# while (i<51):
+#     print(i)
+#     i +=1
+
+
+    # example
+
+# i = 0
+# while(i < 5):
+#     print("Anupam")
+#     i += 1   # or i = i+1
+
+'''
+    Quick Quiz
+    write a program to print the content  of a list using while loops.
+    '''
+
+l =["Anupam", 12, False, "Aniket", True, 434]
+i = 0
+while (i<len(l)):
+    print(l[i])
+    i += 1
