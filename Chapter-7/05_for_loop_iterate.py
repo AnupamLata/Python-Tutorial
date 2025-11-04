b150b70b4b057820206b27d9a495d7aeca3bd9b3
Anupam Lata Yadav <yanu77662@gmail.com>
2025-11-05 00:52:29 +0530

    Add for loop examples for lists, tuples, and strings
    Added examples of for loops with lists, tuples, and strings.

---
+++ Chapter-7/05_for_loop_iterate.py
@@ -0,0 +1,17 @@
+# for loop with lists :
+# l = [ 1, 33, 35, 567, 7, 25]
+# for  i in l :
+#     print(i)
+
+
+# for loop with tuples: 
+
+# t = (6, 7, 8, 9, 5)
+# for i in t:
+#     print(i)   
+
+# for loop for Strings:
+
+s = "Anupam" # , "Aniket", "345"  
+for i in s:
+    print(i)
