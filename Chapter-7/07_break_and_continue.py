ea57a30daed992150d292dd36dbe413db75b3bff
Anupam Lata Yadav <yanu77662@gmail.com>
2025-11-05 00:54:24 +0530

    Implement examples for loop control statements
    Added examples for break, continue, and pass statements.

---
+++ Chapter-7/07_break_and_continue.py
@@ -0,0 +1,24 @@
+# for the Break Statement
+
+# for i in range(1,100):
+#     if (i==34):
+#         break    # Exit the loop right now
+#     print(i)
+    
+    
+# for the continue Statement:
+# for i in range(100):
+#     if (i == 34):
+#         continue  # skip this iteration
+#     print(i)
+
+
+
+    # for the pass Statement
+for i in range (645):
+    pass
+
+i = 0
+while(i<11):
+    print(i)
+    i +=1
