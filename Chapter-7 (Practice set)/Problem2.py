c071c2115bf91e88e9d7f07d145e6de9d5ead17e
Anupam Lata Yadav <yanu77662@gmail.com>
2025-11-06 21:12:21 +0530

    Add greeting program for names starting with 'A'
    Implemented a program to greet names starting with 'A'.

---
+++ Chapter-7 (Practice set)/Problem2.py
@@ -0,0 +1,15 @@
+'''
+Write a program to greet  all the person names stores in a list 'l' and which starts with A.
+l = ["Harry","Aniket","Anupam", "khushi"]
+'''
+
+l = [ "Harry", "Aniket","Anupam", "khushi"]
+
+for name in l:
+    if(name.startswith("A")):
+        print(f"Hello {name}")
+
+
+
+
+
