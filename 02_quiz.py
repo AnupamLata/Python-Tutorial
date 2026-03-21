eaa45a34a3d4969908689bcabce398febba66a17
Anupam Lata Yadav <yanu77662@gmail.com>
2026-03-21 14:23:01 +0530

    Create 02_quiz.py

---
+++ 02_quiz.py
@@ -0,0 +1,7 @@
+# write a program to display the current year , month and day seperately.
+import datetime
+
+today = datetime.date.today()
+print("year : " , today.year)
+print("month : ", today.month)
+print("day : ", today.day)
