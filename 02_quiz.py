aa680e1933865b94517e0accdc9e37530449de31
Anupam Lata Yadav <yanu77662@gmail.com>
2026-03-21 14:23:25 +0530

    Delete 02_quiz.py

--- 02_quiz.py
+++
@@ -1,7 +0,0 @@
-# write a program to display the current year , month and day seperately.
-import datetime
-
-today = datetime.date.today()
-print("year : " , today.year)
-print("month : ", today.month)
-print("day : ", today.day)
