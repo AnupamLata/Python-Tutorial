f4d1652779b53c2bffe039dc4b2f854d01709398
Anupam Lata Yadav <yanu77662@gmail.com>
2026-04-07 00:22:00 +0530

    Create quiz01.py

---
+++ Practice Questions/quiz01.py
@@ -0,0 +1,19 @@
+# How can you concatenate two lists in Python?
+
+a = [1,2,3,4,5]
+b = [6,7,8,9,10]
+
+result = a + b
+
+print(result)
+
+
+
+# Using the extend() method:
+
+c = [5, 6, 7]
+d = [1, 3, 5]
+
+c.extend(d)
+
+print(c)
