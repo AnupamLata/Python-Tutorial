1954e9a74d5b83e18633e3b25a50e24b095bf67e
Anupam Lata Yadav <yanu77662@gmail.com>
2026-09-20 15:52:35 +0530

    Create quiz03.py

---
+++ Practice Questions/quiz03.py
@@ -0,0 +1,6 @@
+# Write a program to print “Hello, Industry!” and display the installed Python version using the `platform` module.
+
+import platform
+
+print("Hello , Industry !")
+print("Installed python Version :", platform.python_version())
