07d74228f93b88463f408774728a22f191fcdc01
Anupam Lata Yadav <yanu77662@gmail.com>
2026-09-21 13:31:55 +0530

    Create quiz06.py

---
+++ Practice Questions/quiz06.py
@@ -0,0 +1,7 @@
+# Create a script that imports the `sys` module and prints the Python interpreter's executable path and version info.
+
+import sys
+
+
+print("Python Executable path", sys.executable)
+print("Python version:", sys.version)
