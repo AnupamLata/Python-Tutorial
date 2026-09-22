dc14c95a462c661609077f830afa2c929f31f4ff
Anupam Lata Yadav <yanu77662@gmail.com>
2026-09-22 14:04:11 +0530

    Create quiz08.py

---
+++ Practice Questions/quiz08.py
@@ -0,0 +1,8 @@
+# Write a program that reads and prints command-line arguments passed to the script using `sys.argv`.
+
+import sys
+
+print("Command line Arguments: ")
+
+for argument in sys.argv:
+    print(argument)
