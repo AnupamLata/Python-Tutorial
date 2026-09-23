39086541bc6748d5724f42496e9e4d5f5a07555b
Anupam Lata Yadav <yanu77662@gmail.com>
2026-09-23 14:04:06 +0530

    Create quiz09.py

---
+++ Practice Questions/quiz09.py
@@ -0,0 +1,17 @@
+# Write a short program (with comments) explaining the key differences between Python2's print statement and Python 3's print function.
+
+
+# Python 2:
+# print was a statement, so parentheses were not  required.
+# print "Hello" 
+
+
+# Python 3:
+# print is a function, so parentheses are required.
+print("Hello")
+
+# Python 3 also allows multiple values inside print()
+print("Hello", "World")
+
+# Python 3 can control the separator between values
+print("Hello", "World", sep=" - ")
