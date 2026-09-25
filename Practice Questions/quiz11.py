c1fa9ede6c58b4e9fcb7a1e24d295b2135e37b5c
Anupam Lata Yadav <yanu77662@gmail.com>
2026-09-25 19:12:15 +0530

    Create quiz11.py

---
+++ Practice Questions/quiz11.py
@@ -0,0 +1,19 @@
+# Write a program to determine and print the datatype of multiple variables: int,float, str,bool, list,tuple,dict,and set
+
+a = 2
+b = 1.2
+c = "Anupam"
+d = False
+e = [1,2,3]
+f = (1,2,3)
+g = {"name": "Anupam", "age": 21}
+h = {1, 2, 3}
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
